51ce318a54e0b2ee05f8cb07beae4ca9dc60d2d4
Sakae Shalom Church <100064296+sakaeshalom@users.noreply.github.com>
2023-08-30 00:47:50 +0900

    Fix making working directory

--- script/cron.sh
+++ script/cron.sh
@@ -4,11 +4,13 @@ set -e
 
 cd -P $(dirname $0)
 cd ../..
-d=$(date -d 'this Sunday' +%Y%m%d)
+d=$(date -d 'next Sunday - 7 days' +%Y%m%d)
 
 if test ! -d $d; then
 	git clone -q bare $d
 	ln -sf $d/script ./
 fi
+
+cd $d
 
 make step0 step1 step2 step3
